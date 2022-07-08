4f7e08d459bc46810567f6a237d50be3eb81cc89
Dan Poulsen <dangjogvara@gmail.com>
2022-07-08 22:15:57 +0100

    return h1

--- challenges/views.py
+++ challenges/views.py
@@ -31,6 +31,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponse("This month is not supported!")
+        return HttpResponse("<h1>This month is not supported!</h1>")
