6bd4f859712a403be7f4c57863761c4733719c6d
Dan Poulsen <dangjogvara@gmail.com>
2022-07-08 15:59:45 +0100

    redirect month number to month

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,35 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+monthly_challenges = {
+    "january": "Run a 5k in the month of January",
+    "february": "Run a 10k in the month of February",
+    "march": "Sleep 8 hours in the month of March",
+    "april": "Learn Django in the month of April",
+    "may": "Eat meat",
+    "june": "Stop eating meat",
+    "july": "Run a 15k in the month of July",
+    "august": "School starts in the month of August",
+    "september": "Final exam in the month of September",
+    "october": "Run a 20k in the month of October",
+    "november": "Learn Python in the month of November",
+    "december": "Finish Django in the month of December",
+
+}
+
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+
+    redirect_month = months[month - 1]
+    return HttpResponseRedirect(redirect_month)
 
 
 def monthly_challenge(request, month):
-    return HttpResponse(month)
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponse("This month is not supported!")
