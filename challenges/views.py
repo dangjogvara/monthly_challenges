e388c60ab7691f38562fc4c34cc14d973452bad0
Dan Poulsen <dangjogvara@gmail.com>
2022-07-09 22:27:59 +0100

    looping through list and base template

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,6 @@
-from ast import arg
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from django.template.loader import render_to_string
+from django.shortcuts import render
 
 monthly_challenges = {
     "january": "Run a 5k in the month of January",
@@ -15,21 +14,16 @@ monthly_challenges = {
     "september": "Final exam in the month of September",
     "october": "Run a 20k in the month of October",
     "november": "Learn Python in the month of November",
-    "december": "Finish Django in the month of December"
+    "december": None
 }
 
 
+# Return list of challenges
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
-
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse('month-challenge', args=[month])
-        list_items += f"<li><a href='{month_path}'>{capitalized_month}</a></li>"
-
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, 'challenges/index.html', {
+        'months': months
+    })
 
 
 def monthly_challenge_by_number(request, month):
@@ -46,7 +40,11 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = render_to_string("challenges/challenge.html")
-        return HttpResponse(response_data)
+        return render(request, 'challenges/challenge.html', {
+            'text': challenge_text,
+            'month_name': month
+
+        })
+
     except:
         return HttpResponse("<h1>This month is not supported!</h1>")
